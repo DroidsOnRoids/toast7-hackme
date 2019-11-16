083e0224abb8a5cc8d37cf20860bccb112c225de
Mikołaj Demków <mikolaj.demkow@meelogic.com>
2019-11-16 14:05:05 +0100

    Fixed answer retrieval
    Answer can be longer / shorter than correct answer so we should not probably limited the retrieval of given answer to correct answer size (even +1) but to the answer length itself

--- app/src/main/c/native-lib.c
+++ app/src/main/c/native-lib.c
@@ -2,12 +2,12 @@
 #include <string.h>
 
 #define CORRECT_ANSWER "donut"
-#define CORRECT_ANSWER_SIZE sizeof(CORRECT_ANSWER)
 
 JNIEXPORT jboolean JNICALL
 Java_pl_droidsonroids_hackme_Utils_isAnswerCorrect(JNIEnv *env, jclass type, jstring answer_) {
-	char answer[CORRECT_ANSWER_SIZE + 1];
-	(*env)->GetStringUTFRegion(env, answer_, 0, CORRECT_ANSWER_SIZE + 1, answer);
+	int answerLength = (*env)->GetStringUTFLength(env, answer_);
+	char answer[answerLength];
+	(*env)->GetStringUTFRegion(env, answer_, 0, answerLength, answer);
 
 	if (strcmp(answer, CORRECT_ANSWER) == 0) {
 		return JNI_TRUE;
