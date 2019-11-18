ef01cfc70701212e315cf2887c054ed003b03c1f
Mikołaj Demków <mikolaj.demkow@meelogic.com>
2019-11-18 09:32:57 +0100

    Added string termination

--- app/src/main/c/native-lib.c
+++ app/src/main/c/native-lib.c
@@ -5,9 +5,10 @@
 
 JNIEXPORT jboolean JNICALL
 Java_pl_droidsonroids_hackme_Utils_isAnswerCorrect(JNIEnv *env, jclass type, jstring answer_) {
-	int answerLength = (*env)->GetStringUTFLength(env, answer_);
-	char answer[answerLength];
+	jsize answerLength = (*env)->GetStringUTFLength(env, answer_);
+	char answer[answerLength + 1];
 	(*env)->GetStringUTFRegion(env, answer_, 0, answerLength, answer);
+	answer[answerLength] = '\0';
 
 	if (strcmp(answer, CORRECT_ANSWER) == 0) {
 		return JNI_TRUE;
